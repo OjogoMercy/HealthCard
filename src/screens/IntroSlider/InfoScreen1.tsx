0c826697f0c65d2b088219be606acd903a47ed0f
OjogoMercy <mercyojogo@gmail.com>
2025-12-31 03:03:41 -0800

    Aded Screens

---
+++ src/screens/IntroSlider/InfoScreen1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfoScreen1 = () => {
+  return (
+    <View>
+      <Text>InfoScreen1</Text>
+    </View>
+  )
+}
+
+export default InfoScreen1
+
+const styles = StyleSheet.create({})
---
+++ src/screens/IntroSlider/InfoScreen2.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfoScreen2 = () => {
+  return (
+    <View>
+      <Text>InfoScreen2</Text>
+    </View>
+  )
+}
+
+export default InfoScreen2
+
+const styles = StyleSheet.create({})
