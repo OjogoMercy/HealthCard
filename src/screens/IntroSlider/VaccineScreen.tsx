e3817af927c24c789254a8c771b3192e80f02768
OjogoMercy <mercyojogo@gmail.com>
2025-12-31 04:28:01 -0800

    first commit

---
+++ src/screens/IntroSlider/VaccineScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VaccineScreen = () => {
+  return (
+    <View>
+      <Text>VaccineScreen</Text>
+    </View>
+  )
+}
+
+export default VaccineScreen
+
+const styles = StyleSheet.create({})
