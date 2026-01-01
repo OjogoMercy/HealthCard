9aa87465623891f9839aebca6b34487bfdd057ad
OjogoMercy <mercyojogo@gmail.com>
2026-01-01 01:27:12 -0800

    Remove tab layout and explore screens; update root layout to use RootNavigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
+import RootNavigator from "../src/navigation/RootNavigator";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <RootNavigator />;
 }
