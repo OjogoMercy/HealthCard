64b1a86ee67def24950e7a1deb8be39ba5eda027
OjogoMercy <mercyojogo@gmail.com>
2026-01-13 15:38:49 +0100

    cew jcew

--- src/constants/ThemedText.tsx
+++ src/constants/ThemedText.tsx
@@ -1,76 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { Text, type TextProps, StyleSheet } from 'react-native'
-import { COLORS, FONTS } from './THEME'
+import React from "react";
+import { StyleSheet, Text, type TextProps } from "react-native";
+import { COLORS, FONTS } from "./THEME";
 
 export type ThemedTextProps = TextProps & {
-  lightColor?: string
-  darkColor?: string
+  lightColor?: string;
+  darkColor?: string;
   type?:
-    | 'text1'
-    | 'text1bold'
-    | 'text2'
-    | 'text2bold'
-    | 'text2boldwhite'
-    | 'text3'
-    | 'text3bold'
-    | 'text3gray'
-    | 'text3white'
-    | 'text3boldwhite'
-    | 'text3green'
-    | 'text4'
-    | 'text4bold'
-    | 'text4gray'
-    | 'text4white'
-    | 'text4green'
-    | 'text4greenbold'
-    | 'text5'
-    | 'text5bold'
-    | 'text5white'
-    | 'text6'
-}
+    | "text1"
+    | "text1bold"
+    | "text2"
+    | "text2bold"
+    | "text2boldwhite"
+    | "text3"
+    | "text3bold"
+    | "text3gray"
+    | "text3white"
+    | "text3boldwhite"
+    | "text3green"
+    | "text4"
+    | "text4bold"
+    | "text4gray"
+    | "text4white"
+    | "text4green"
+    | "text4greenbold"
+    | "text5"
+    | "text5bold"
+    | "text5white"
+    | "text6";
+};
 
 export function ThemedText({
   style,
   lightColor,
   darkColor,
-  type = 'text3',
+  type = "text3",
   children,
   onPress,
   ...rest
 }: ThemedTextProps) {
-  const translatedText = typeof children === 'string' ? children : children
+  const translatedText = typeof children === "string" ? children : children;
 
   return (
     <Text
       onPress={onPress}
       style={[
-        type === 'text1' ? styles.text1 : undefined,
-        type === 'text1bold' ? styles.text1bold : undefined,
-        type === 'text2' ? styles.text2 : undefined,
-        type === 'text2bold' ? styles.text2bold : undefined,
-        type === 'text2boldwhite' ? styles.text2boldwhite : undefined,
-        type === 'text3' ? styles.text3 : undefined,
-        type === 'text3bold' ? styles.text3bold : undefined,
-        type === 'text3gray' ? styles.text3gray : undefined,
-        type === 'text3boldwhite' ? styles.text3boldwhite : undefined,
-        type === 'text3white' ? styles.text3white : undefined,
-        type === 'text3green' ? styles.text3green : undefined,
-        type === 'text4' ? styles.text4 : undefined,
-        type === 'text4bold' ? styles.text4bold : undefined,
-        type === 'text4gray' ? styles.text4gray : undefined,
-        type === 'text4white' ? styles.text4white : undefined,
-        type === 'text4green' ? styles.text4green : undefined,
-        type === 'text4greenbold' ? styles.text4greenbold : undefined,
-        type === 'text5' ? styles.text5 : undefined,
-        type === 'text5bold' ? styles.text5bold : undefined,
-        type === 'text5white' ? styles.text5white : undefined,
-        type === 'text6' ? styles.text6 : undefined,
+        type === "text1" ? styles.text1 : undefined,
+        type === "text1bold" ? styles.text1bold : undefined,
+        type === "text2" ? styles.text2 : undefined,
+        type === "text2bold" ? styles.text2bold : undefined,
+        type === "text2boldwhite" ? styles.text2boldwhite : undefined,
+        type === "text3" ? styles.text3 : undefined,
+        type === "text3bold" ? styles.text3bold : undefined,
+        type === "text3gray" ? styles.text3gray : undefined,
+        type === "text3boldwhite" ? styles.text3boldwhite : undefined,
+        type === "text3white" ? styles.text3white : undefined,
+        type === "text3green" ? styles.text3green : undefined,
+        type === "text4" ? styles.text4 : undefined,
+        type === "text4bold" ? styles.text4bold : undefined,
+        type === "text4gray" ? styles.text4gray : undefined,
+        type === "text4white" ? styles.text4white : undefined,
+        type === "text4green" ? styles.text4green : undefined,
+        type === "text4greenbold" ? styles.text4greenbold : undefined,
+        type === "text5" ? styles.text5 : undefined,
+        type === "text5bold" ? styles.text5bold : undefined,
+        type === "text5white" ? styles.text5white : undefined,
+        type === "text6" ? styles.text6 : undefined,
         style,
       ]}
-      {...rest}>
+      {...rest}
+    >
       {translatedText}
     </Text>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -97,7 +98,7 @@ const styles = StyleSheet.create({
   text2boldwhite: {
     ...FONTS.h2bold,
     color: COLORS.white,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   text3: {
     ...FONTS.h3,
@@ -107,12 +108,12 @@ const styles = StyleSheet.create({
   text3bold: {
     ...FONTS.h3bold,
     color: COLORS.black,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   text3boldwhite: {
     ...FONTS.h3bold,
     color: COLORS.white,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   text3gray: {
     ...FONTS.h3,
@@ -137,7 +138,7 @@ const styles = StyleSheet.create({
   text4bold: {
     ...FONTS.h4bold,
     color: COLORS.black,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   text4gray: {
     ...FONTS.h4,
@@ -157,7 +158,7 @@ const styles = StyleSheet.create({
   text4greenbold: {
     ...FONTS.h4bold,
     color: COLORS.primary,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   text5: {
     ...FONTS.h5,
@@ -167,7 +168,7 @@ const styles = StyleSheet.create({
   text5bold: {
     ...FONTS.h5bold,
     color: COLORS.black,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   text5white: {
     ...FONTS.h5,
@@ -179,4 +180,4 @@ const styles = StyleSheet.create({
     color: COLORS.black,
     fontWeight: FONTS.h6.fontWeight as any,
   },
-})
+});
