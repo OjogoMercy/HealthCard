bf156ab9a903dead523f958c6ec27dccbe52cadb
OjogoMercy <mercyojogo@gmail.com>
2026-03-03 15:59:40 +0100

    feat: enhance WrapView with navigation and screen title support; update ProfileScreen to utilize new screenTitle prop

--- src/components/WrapView.tsx
+++ src/components/WrapView.tsx
@@ -1,13 +1,17 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "expo-router";
 import React from "react";
-import { StatusBar, StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { COLORS, SIZES } from "../constants/THEME";
-
+import { ThemedText } from "../constants/ThemedText";
 interface WrapViewProps {
   children: React.ReactNode;
   style: object;
+  screenTitle?: string;
 }
-const WrapView = ({ children, style }: WrapViewProps) => {
+const WrapView = ({ children, style, screenTitle }: WrapViewProps) => {
+  const navigation = useNavigation();
   return (
     <SafeAreaProvider style={[styles.container, style]}>
       <StatusBar
@@ -17,6 +21,20 @@ const WrapView = ({ children, style }: WrapViewProps) => {
       />
 
       <View style={{ alignItems: "center", flex: 1, justifyContent: "center" }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => navigation.goBack()}
+          >
+            <Ionicons name="chevron-back" size={22} color={"black"} />
+          </TouchableOpacity>
+          <ThemedText type="text2bold" style={{ color: COLORS.primary }}>
+            {screenTitle}
+          </ThemedText>
+          <TouchableOpacity activeOpacity={0.7} onPress={undefined}>
+            <Ionicons name="list" size={22} color={"black"} />
+          </TouchableOpacity>
+        </View>
         {children}
       </View>
     </SafeAreaProvider>
--- src/screens/BottomScreens/Profile/ProfileScreen.tsx
+++ src/screens/BottomScreens/Profile/ProfileScreen.tsx
@@ -1,17 +1,12 @@
 import WrapView from "@/src/components/WrapView";
-import { ThemedText } from "@/src/constants/ThemedText";
 import React from "react";
 import { StyleSheet } from "react-native";
-import {COLORS} from '../../../constants/THEME'
 
 const ProfileScreen = () => {
   return (
-    <WrapView>
-      <ThemedText type="text2bold" style={{ color: COLORS.primary }}>
-        Profile
-      </ThemedText>
-      
-    </WrapView>
+    <WrapView      
+      screenTitle="Profile"
+    ></WrapView>
   );
 };
 
