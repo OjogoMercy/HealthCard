8ffb13d51fbbcb294dfb00b98248fc2074cb1f3c
OjogoMercy <mercyojogo@gmail.com>
2026-04-13 14:01:59 +0100

    Add CatchUpFlow screen with basic structure and styling

---
+++ src/screens/BottomScreens/Home/CatchUpFlow.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CatchUpFlow = () => {
+  return (
+    <View>
+      <Text>CatchUpFlow</Text>
+    </View>
+  )
+}
+
+export default CatchUpFlow
+
+const styles = StyleSheet.create({})
